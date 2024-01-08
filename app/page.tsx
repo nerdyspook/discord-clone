1b39fa387c2b36a46582a029b0e49641a857faaa
nerdyspook <sushantomahato1@gmail.com>
2024-01-08 23:01:10 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl">Hello World</p>
+      <Button>Click here</Button>
+    </div>
+  );
+}
